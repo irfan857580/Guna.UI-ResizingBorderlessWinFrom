6bd02386ca29277227e157c6abc512bff562e6a8
Sobatdata Software <sobatdata.software@gmail.com>
2019-08-28 03:35:05 +0700

    init

---
+++ ResizingBorderlessVB/ResizingBorderless/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ResizingBorderless
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GunaResize1 = new Guna.UI.WinForms.GunaResize(this.components);
+            this.GunaDragControl1 = new Guna.UI.WinForms.GunaDragControl(this.components);
+            this.GunaPictureBox1 = new Guna.UI.WinForms.GunaPictureBox();
+            this.GunaControlBox3 = new Guna.UI.WinForms.GunaControlBox();
+            this.GunaControlBox2 = new Guna.UI.WinForms.GunaControlBox();
+            this.GunaControlBox1 = new Guna.UI.WinForms.GunaControlBox();
+            ((System.ComponentModel.ISupportInitialize)(this.GunaPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GunaResize1
+            // 
+            this.GunaResize1.TargetForm = this;
+            // 
+            // GunaDragControl1
+            // 
+            this.GunaDragControl1.TargetControl = this;
+            // 
+            // GunaPictureBox1
+            // 
+            this.GunaPictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.GunaPictureBox1.BaseColor = System.Drawing.Color.White;
+            this.GunaPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("GunaPictureBox1.Image")));
+            this.GunaPictureBox1.Location = new System.Drawing.Point(120, 106);
+            this.GunaPictureBox1.Name = "GunaPictureBox1";
+            this.GunaPictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.GunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.GunaPictureBox1.TabIndex = 7;
+            this.GunaPictureBox1.TabStop = false;
+            // 
+            // GunaControlBox3
+            // 
+            this.GunaControlBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.GunaControlBox3.Animated = true;
+            this.GunaControlBox3.AnimationHoverSpeed = 0.07F;
+            this.GunaControlBox3.AnimationSpeed = 0.03F;
+            this.GunaControlBox3.ControlBoxType = Guna.UI.WinForms.FormControlBoxType.MinimizeBox;
+            this.GunaControlBox3.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(85)))), ((int)(((byte)(244)))));
+            this.GunaControlBox3.IconSize = 12F;
+            this.GunaControlBox3.Location = new System.Drawing.Point(255, 4);
+            this.GunaControlBox3.Name = "GunaControlBox3";
+            this.GunaControlBox3.OnHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(35)))), ((int)(((byte)(63)))));
+            this.GunaControlBox3.OnHoverIconColor = System.Drawing.Color.White;
+            this.GunaControlBox3.OnPressedColor = System.Drawing.Color.Black;
+            this.GunaControlBox3.Size = new System.Drawing.Size(30, 30);
+            this.GunaControlBox3.TabIndex = 6;
+            // 
+            // GunaControlBox2
+            // 
+            this.GunaControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.GunaControlBox2.Animated = true;
+            this.GunaControlBox2.AnimationHoverSpeed = 0.07F;
+            this.GunaControlBox2.AnimationSpeed = 0.03F;
+            this.GunaControlBox2.ControlBoxType = Guna.UI.WinForms.FormControlBoxType.MaximizeBox;
+            this.GunaControlBox2.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(85)))), ((int)(((byte)(244)))));
+            this.GunaControlBox2.IconSize = 12F;
+            this.GunaControlBox2.Location = new System.Drawing.Point(291, 4);
+            this.GunaControlBox2.Name = "GunaControlBox2";
+            this.GunaControlBox2.OnHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(35)))), ((int)(((byte)(63)))));
+            this.GunaControlBox2.OnHoverIconColor = System.Drawing.Color.White;
+            this.GunaControlBox2.OnPressedColor = System.Drawing.Color.Black;
+            this.GunaControlBox2.Size = new System.Drawing.Size(30, 30);
+            this.GunaControlBox2.TabIndex = 5;
+            // 
+            // GunaControlBox1
+            // 
+            this.GunaControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.GunaControlBox1.Animated = true;
+            this.GunaControlBox1.AnimationHoverSpeed = 0.07F;
+            this.GunaControlBox1.AnimationSpeed = 0.03F;
+            this.GunaControlBox1.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(85)))), ((int)(((byte)(244)))));
+            this.GunaControlBox1.IconSize = 12F;
+            this.GunaControlBox1.Location = new System.Drawing.Point(327, 4);
+            this.GunaControlBox1.Name = "GunaControlBox1";
+            this.GunaControlBox1.OnHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(35)))), ((int)(((byte)(63)))));
+            this.GunaControlBox1.OnHoverIconColor = System.Drawing.Color.White;
+            this.GunaControlBox1.OnPressedColor = System.Drawing.Color.Black;
+            this.GunaControlBox1.Size = new System.Drawing.Size(30, 30);
+            this.GunaControlBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(35)))), ((int)(((byte)(63)))));
+            this.ClientSize = new System.Drawing.Size(360, 329);
+            this.Controls.Add(this.GunaPictureBox1);
+            this.Controls.Add(this.GunaControlBox3);
+            this.Controls.Add(this.GunaControlBox2);
+            this.Controls.Add(this.GunaControlBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GunaPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Guna.UI.WinForms.GunaResize GunaResize1;
+        internal Guna.UI.WinForms.GunaPictureBox GunaPictureBox1;
+        internal Guna.UI.WinForms.GunaControlBox GunaControlBox3;
+        internal Guna.UI.WinForms.GunaControlBox GunaControlBox2;
+        internal Guna.UI.WinForms.GunaControlBox GunaControlBox1;
+        internal Guna.UI.WinForms.GunaDragControl GunaDragControl1;
+    }
+}
+
---
+++ ResizingBorderlessVB/ResizingBorderlessVB/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GunaResize1 = New Guna.UI.WinForms.GunaResize(Me.components)
+        Me.GunaDragControl1 = New Guna.UI.WinForms.GunaDragControl(Me.components)
+        Me.GunaControlBox1 = New Guna.UI.WinForms.GunaControlBox()
+        Me.GunaControlBox2 = New Guna.UI.WinForms.GunaControlBox()
+        Me.GunaControlBox3 = New Guna.UI.WinForms.GunaControlBox()
+        Me.GunaPictureBox1 = New Guna.UI.WinForms.GunaPictureBox()
+        CType(Me.GunaPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GunaResize1
+        '
+        Me.GunaResize1.TargetForm = Me
+        '
+        'GunaDragControl1
+        '
+        Me.GunaDragControl1.TargetControl = Me
+        '
+        'GunaControlBox1
+        '
+        Me.GunaControlBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaControlBox1.Animated = True
+        Me.GunaControlBox1.AnimationHoverSpeed = 0.07!
+        Me.GunaControlBox1.AnimationSpeed = 0.03!
+        Me.GunaControlBox1.IconColor = System.Drawing.Color.FromArgb(CType(CType(96, Byte), Integer), CType(CType(85, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.GunaControlBox1.IconSize = 12.0!
+        Me.GunaControlBox1.Location = New System.Drawing.Point(384, 4)
+        Me.GunaControlBox1.Name = "GunaControlBox1"
+        Me.GunaControlBox1.OnHoverBackColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(63, Byte), Integer))
+        Me.GunaControlBox1.OnHoverIconColor = System.Drawing.Color.White
+        Me.GunaControlBox1.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaControlBox1.Size = New System.Drawing.Size(30, 30)
+        Me.GunaControlBox1.TabIndex = 0
+        '
+        'GunaControlBox2
+        '
+        Me.GunaControlBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaControlBox2.Animated = True
+        Me.GunaControlBox2.AnimationHoverSpeed = 0.07!
+        Me.GunaControlBox2.AnimationSpeed = 0.03!
+        Me.GunaControlBox2.ControlBoxType = Guna.UI.WinForms.FormControlBoxType.MaximizeBox
+        Me.GunaControlBox2.IconColor = System.Drawing.Color.FromArgb(CType(CType(96, Byte), Integer), CType(CType(85, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.GunaControlBox2.IconSize = 12.0!
+        Me.GunaControlBox2.Location = New System.Drawing.Point(348, 4)
+        Me.GunaControlBox2.Name = "GunaControlBox2"
+        Me.GunaControlBox2.OnHoverBackColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(63, Byte), Integer))
+        Me.GunaControlBox2.OnHoverIconColor = System.Drawing.Color.White
+        Me.GunaControlBox2.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaControlBox2.Size = New System.Drawing.Size(30, 30)
+        Me.GunaControlBox2.TabIndex = 1
+        '
+        'GunaControlBox3
+        '
+        Me.GunaControlBox3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaControlBox3.Animated = True
+        Me.GunaControlBox3.AnimationHoverSpeed = 0.07!
+        Me.GunaControlBox3.AnimationSpeed = 0.03!
+        Me.GunaControlBox3.ControlBoxType = Guna.UI.WinForms.FormControlBoxType.MinimizeBox
+        Me.GunaControlBox3.IconColor = System.Drawing.Color.FromArgb(CType(CType(96, Byte), Integer), CType(CType(85, Byte), Integer), CType(CType(244, Byte), Integer))
+        Me.GunaControlBox3.IconSize = 12.0!
+        Me.GunaControlBox3.Location = New System.Drawing.Point(312, 4)
+        Me.GunaControlBox3.Name = "GunaControlBox3"
+        Me.GunaControlBox3.OnHoverBackColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(63, Byte), Integer))
+        Me.GunaControlBox3.OnHoverIconColor = System.Drawing.Color.White
+        Me.GunaControlBox3.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaControlBox3.Size = New System.Drawing.Size(30, 30)
+        Me.GunaControlBox3.TabIndex = 2
+        '
+        'GunaPictureBox1
+        '
+        Me.GunaPictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.GunaPictureBox1.BaseColor = System.Drawing.Color.White
+        Me.GunaPictureBox1.Image = CType(resources.GetObject("GunaPictureBox1.Image"), System.Drawing.Image)
+        Me.GunaPictureBox1.Location = New System.Drawing.Point(150, 122)
+        Me.GunaPictureBox1.Name = "GunaPictureBox1"
+        Me.GunaPictureBox1.Size = New System.Drawing.Size(120, 120)
+        Me.GunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.GunaPictureBox1.TabIndex = 3
+        Me.GunaPictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(63, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(419, 379)
+        Me.Controls.Add(Me.GunaPictureBox1)
+        Me.Controls.Add(Me.GunaControlBox3)
+        Me.Controls.Add(Me.GunaControlBox2)
+        Me.Controls.Add(Me.GunaControlBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.GunaPictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GunaResize1 As Guna.UI.WinForms.GunaResize
+    Friend WithEvents GunaDragControl1 As Guna.UI.WinForms.GunaDragControl
+    Friend WithEvents GunaControlBox3 As Guna.UI.WinForms.GunaControlBox
+    Friend WithEvents GunaControlBox2 As Guna.UI.WinForms.GunaControlBox
+    Friend WithEvents GunaControlBox1 As Guna.UI.WinForms.GunaControlBox
+    Friend WithEvents GunaPictureBox1 As Guna.UI.WinForms.GunaPictureBox
+End Class
